1938166829d7e5c9124fd2c5144045fb1ca9be15
Katie Fenn <masqurinwizard@googlemail.com>
2014-12-16 22:04:09 +0000

    Installed grunt-mocha-test
    - Installed grunt-mocha-test
    - Added mochaTest task to grunt dev

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,16 +9,25 @@ module.exports = function(grunt) {
                 node: true,
             }
         },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                },
+                src: ['test/*.js']
+            }
+        },
         watch: {
             jshint: {
                 files: ['src/*.js'],
-                tasks: ['jshint:all']
+                tasks: ['jshint:all','mochaTest:test']
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-mocha-test');
 
     grunt.registerTask('dev', ['watch']);
 
